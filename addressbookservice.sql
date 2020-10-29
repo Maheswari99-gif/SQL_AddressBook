dede195129a2896202537e5d66d4c53e8545f6cc
Maheswari99-gif <maheswarikoyyani999@gmail.com>
2020-10-29 11:00:57 +0530

    MaheswariKoyyani : Creating a table

--- addressbookservice.sql
+++ addressbookservice.sql
@@ -1,3 +1,19 @@
 #UC1
 CREATE DATABASE address_book_service;
 SHOW databases;
+USE employee_payroll;
+select database();
+#UC2
+CREATE TABLE address_book
+(
+first_name VARCHAR(80) NOT NULL,
+last_name VARCHAR(80) NOT NULL,
+address VARCHAR(150) NOT NULL,
+city VARCHAR(50) NOT NULL,
+state VARCHAR(50) NOT NULL,
+zip VARCHAR(6) NOT NULL,
+phone VARCHAR(10) NOT NULL,
+email VARCHAR(100) NOT NULL
+);
+DESCRIBE address_book;
+
