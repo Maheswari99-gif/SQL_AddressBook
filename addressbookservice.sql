651236eb70a3387e9723ebc3e58e593f96002832
Maheswari99-gif <maheswarikoyyani999@gmail.com>
2020-10-29 11:10:44 +0530

    MaheswariKoyyani : Insert into function and select all things

--- addressbookservice.sql
+++ addressbookservice.sql
@@ -16,4 +16,11 @@ phone VARCHAR(10) NOT NULL,
 email VARCHAR(100) NOT NULL
 );
 DESCRIBE address_book;
+#UC3
+INSERT INTO address_book VALUES
+( 'Bill', 'Gates', 'House No.-1', 'City A', 'State A', '123456', '1234567890', 'billgates@email.com' ),
+( 'Mark', 'Zuckerberg', 'House No.-2', 'City B','State B', '345677', '2345678912', 'markzukerberg@email.com' ),
+( 'Ambani', 'Mukesh', 'House No.-3', 'City C', 'State C', '986754', '3456789102', 'ambanimukesh@email.com' );
+SELECT * FROM address_book;
+
 
